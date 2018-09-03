8daa25073ea0f7e6f99c141d382c3b22b223a393
root <root@localhost.localdomain>
2018-09-03 10:09:38 +0300

    Articles class

--- models/news.py
+++ models/news.py
@@ -1,21 +1,4 @@
 class News:
-    '''
-    News class to define News Objects
-    '''
-
-    def __init__(self,id,name,description,url,category,language,country):
-        self.id = id
-        self.name = name
-        self.description = description
-        self.url = url
-        self.category = category
-        self.language = language
-        self.country = country
-class News:
-    '''
-    Newsclass to define News Objects
-    '''
-
     def __init__(self,id,title,overview,url,category,country):
         self.id =id
         self.title = title
@@ -23,3 +6,12 @@ class News:
         self.url = url
         self.category = category
         self.country = country
+
+class Articles:
+    def __init__(self,author,title,description,url,urlToImage,publishedAt):
+        self.author = author
+        self.title = title
+        self.description = descrition
+        self.url = url
+        self.urlToImage = urlToImage
+        self.publishedAt = publishedAt
