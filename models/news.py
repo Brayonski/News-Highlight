2933e55ce6c5165a1918bf9ff18a78d6de0959d7
root <root@localhost.localdomain>
2018-09-03 08:20:35 +0300

    change in models location

---
+++ models/news.py
@@ -0,0 +1,25 @@
+class News:
+    '''
+    News class to define News Objects
+    '''
+
+    def __init__(self,id,name,description,url,category,language,country):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.url = url
+        self.category = category
+        self.language = language
+        self.country = country
+class News:
+    '''
+    Newsclass to define News Objects
+    '''
+
+    def __init__(self,id,title,overview,url,category,country):
+        self.id =id
+        self.title = title
+        self.overview = overview
+        self.url = url
+        self.category = category
+        self.country = country
