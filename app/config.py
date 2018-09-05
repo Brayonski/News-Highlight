f0c7201140f10b5f5bd3afbf1efab357c7ff144d
root <root@localhost.localdomain>
2018-09-05 07:52:04 +0300

    articles api base url

--- app/config.py
+++ app/config.py
@@ -4,6 +4,7 @@ class Config:
     '''
 
     NEWS_API_BASE_URL ='https://newsapi.org/v2/sources?category{}&apiKey={}'
+    ARTICLES_API_BASE_URL ='https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
     pass
 
 
