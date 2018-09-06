9203bc61a7ca07465fb485187610b4c4e7493764
root <root@localhost.localdomain>
2018-09-06 08:23:41 +0300

    modified content

--- app/views.py
+++ app/views.py
@@ -15,20 +15,28 @@ def index():
     title = 'Home - Welcome to The best News  Website Online'
     return render_template('index.html', title = title, popular = popular_news)
 
-@app.route('/')
-def index():
-    '''
-    View root page function that returns the articles page and its data
-    '''
-    #Getting articles news feed
-    news_articles = get_articles('myarticles')
-    print(news_articles)
-    title = 'Articles'
-    return render_template('articles.html', title = title, myartcles = news_articles)
-
 @app.route('/news/<int:news_id>')
 def news(news_id):
     '''
     View news page function that returns the news details page and its data
     '''
     return render_template('news.html', id=news_id)
+
+# @app.route('/')
+# def articles():
+#     '''
+#     View root page function that returns the articles page and its data
+#     '''
+#     #Getting articles news feed
+#     news_articles = get_articles('myarticles')
+#     print(news_articles)
+#     title = 'Articles'
+#     return render_template('articles.html', title = title, myartcles = news_articles)
+
+# @app.route('/articles/<int:articles_id>')
+# def articles(articles_id):
+#     '''
+#     View articles page function that returns the articles page and its data
+#     '''
+#     return render_template('articles.html', id = articles_id)
+
