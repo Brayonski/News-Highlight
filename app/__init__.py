403453d03a40c5f7f0496c24644d9c6d753ae424
root <root@localhost.localdomain>
2018-09-06 16:17:57 +0300

    flask script,flask & flask bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,4 @@
 from flask import Flask
-from config import DevConfig
 from config import config_options
 from flask_bootstrap import Bootstrap
 
--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config:
 
     SOURCES_API_BASE_URL ='https://newsapi.org/v2/sources?category{}&apiKey={}'
     ARTICLES_API_BASE_URL = 'https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
-    API_KEY = os.environ.get('NEWS_API_KEY')
+    API_KEY = os.environ.get('API_KEY')
     SECRET_KEY = os.environ.get('SECRET_KEY')
     pass
 
