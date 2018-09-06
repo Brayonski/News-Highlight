d1d77904865093b94af1af117047cd31a5d616dc
root <root@localhost.localdomain>
2018-09-06 07:32:18 +0300

    class for articles

---
+++ models/articles.py
@@ -0,0 +1,9 @@
+class Articles:
+    def __init__(self,id,author,title,description,url,urlToImage,publishedAt):
+        self.id = id
+        self.author = author
+        self.title = title
+        self.description = descrition
+        self.url = url
+        self.urlToImage = urlToImage
+        self.publishedAt = publishedAt
